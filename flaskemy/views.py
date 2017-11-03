693c4fc21a0e5a65697e4e09de996504e493ce32
Shaphil <shaphil2025@gmail.com>
2017-11-03 23:49:39 +0600

    Show tasks from database

--- flaskemy/views.py
+++ flaskemy/views.py
@@ -1,8 +1,11 @@
 from flask import render_template
 
-from flaskemy import app
+from flaskemy import app, db
+from flaskemy.models import Task
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # there should be a sanity check in case the db doesn't exist
+    tasks = db.session.query(Task)
+    return render_template('index.html', tasks=tasks)
