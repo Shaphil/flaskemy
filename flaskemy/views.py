7b41c0133cc053ded5ac25b91e038ff07c015e6c
Shaphil <shaphil2025@gmail.com>
2017-11-04 00:23:01 +0600

    Add method for adding new task

--- flaskemy/views.py
+++ flaskemy/views.py
@@ -1,11 +1,17 @@
-from flask import render_template
+from flask import render_template, request
 
 from flaskemy import app, db
 from flaskemy.models import Task
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        # Add validity check for task name
+        task = Task(name=request.form['name'])
+        db.session.add(task)
+        db.session.commit()
+
     # there should be a sanity check in case the db doesn't exist
     tasks = db.session.query(Task)
     return render_template('index.html', tasks=tasks)
