fd62cfc8efd49062c0c43f70672d3af430f3f1a6
Shaphil <shaphil2025@gmail.com>
2017-11-03 22:36:06 +0600

    Add configurations

--- flaskemy/__init__.py
+++ flaskemy/__init__.py
@@ -1,7 +1,20 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Application configuration
+DB_NAME = 'flaskemy.db'
+DB_PATH = os.path.join(basedir, DB_NAME)
+
+# SQLAlchemy configuration
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DB_PATH
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+db = SQLAlchemy(app)
 
 import flaskemy.views
