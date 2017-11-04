8224935ca411d71315ffeb3ba98ed920f3390d3e
Shaphil <shaphil2025@gmail.com>
2017-11-04 15:05:56 +0600

    Add user login

--- flaskemy/views/auth.py
+++ flaskemy/views/auth.py
@@ -1,7 +1,7 @@
 from flask import render_template, request
 
 from flaskemy import app, db
-from flaskemy.models import User
+from flaskemy.models import User, Task
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -15,3 +15,16 @@ def register():
         db.session.commit()
 
     return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form['user_name']
+        password = request.form['user_password']
+        user = User.query.filter_by(name=name).first()
+        if user and password == user.password:
+            tasks = Task.query.filter_by(user_id=user.id).all()
+            return render_template('index.html', tasks=tasks)
+
+    return render_template('login.html')
