9bccac5e6b382eeb9e0a359f5d3b4a7d8c4b607e
Shaphil <shaphil2025@gmail.com>
2017-11-04 12:35:59 +0600

    Add User model

--- flaskemy/models.py
+++ flaskemy/models.py
@@ -3,10 +3,22 @@ from datetime import datetime
 from flaskemy import db
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password = db.Column(db.String(32), nullable=False)
+    tasks = db.relationship('Task', backref='user', lazy=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True, nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return '<Task %r>' % self.name
